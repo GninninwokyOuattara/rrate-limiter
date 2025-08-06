ffa9732152ce319ea3c9372abc10bf9b66352f2f
g-ouattara <idrissgouattara@gmail.com>
2025-08-06 13:59:02 +0000

    no more unwraping around, the handler return a result type where the ok and err can be converted into response.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use anyhow::Context;
 use axum::{
     Router,
     extract::{Query, Request, State},
@@ -10,6 +11,7 @@ use axum::{
     routing::get,
 };
 
+mod errors;
 mod rate_limiter;
 mod rules;
 mod utils;
@@ -68,7 +70,7 @@ async fn limiter_handler(
     Query(params): Query<HashMap<String, String>>,
     State(states): State<Arc<States>>,
     request: Request,
-) -> impl IntoResponse {
+) -> anyhow::Result<impl IntoResponse, errors::LimiterError> {
     println!("Received params: {:?}", params);
     let uri = request.uri();
     let headers = request.headers();
@@ -76,24 +78,17 @@ async fn limiter_handler(
     // Finding which pattern match the uri using the matcher
 
     // We use the router matcher for that
-    let matched_route = states
-        .route_matcher
-        .lock()
-        .unwrap()
-        .at(uri.path())
-        .unwrap()
-        .value
-        .clone();
+    let matched_route = states.route_matcher.lock()?.at(uri.path())?.value.clone();
     println!("The matching route : {:#?}", &matched_route);
 
     // Find the algorithm for that route in the redis cache.
 
     let algorithm = states
         .redis_connection
-        .lock()
-        .unwrap()
+        .lock()?
         .get::<&String, String>(&format!("rules_to_algorithms:{}", &matched_route))
-        .unwrap();
+        .context("Failed to succesfully retrieve the redis key")?;
+
     println!("Algorithm found: {:#?}", algorithm);
 
     // Where the rate limiting happens.
@@ -105,7 +100,7 @@ async fn limiter_handler(
     )
     .unwrap();
 
-    (axum::http::StatusCode::OK, headers.to_headers(), message)
+    Ok((axum::http::StatusCode::OK, headers.to_headers(), message))
 
     // println!("Request: {:#?}", request);
 
