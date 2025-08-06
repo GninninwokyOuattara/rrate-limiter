7197aa359c36fda4f6743fe53cbd52d267d330de
g-ouattara <idrissgouattara@gmail.com>
2025-08-06 10:24:01 +0000

    a method to prepopulatea the redis stoe with some mapping between rule and the rate limiting algorithm

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,21 @@
-use crate::rate_limiter::RateLimiterAlgorithms;
+use redis::{Commands, RedisError};
+
+use crate::{rate_limiter::RateLimiterAlgorithms, rules::Rule};
 
 pub fn make_redis_key(key: &str, endpoint: &str, algorithm: &RateLimiterAlgorithms) -> String {
+    // Ex : fixed_window:userid:matched_endpoint_from_rules
     format!("{}:{}:{}", algorithm.to_string(), key, endpoint)
+}
+
+pub fn populate_redis_kv_rule_algorithm(
+    conn: &mut redis::Connection,
+    rules: &Vec<Rule>,
+) -> Result<(), RedisError> {
+    for rule in rules {
+        conn.set(
+            format!("rules_to_algorithms:{}", rule.hash.clone()),
+            rule.algorithm.to_string(),
+        )?
+    }
+    Ok(())
 }
