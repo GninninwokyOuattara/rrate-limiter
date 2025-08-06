30187b7617e2fc412d177b84ecc7e3eba8e5d1d9
g-ouattara <idrissgouattara@gmail.com>
2025-08-06 13:58:24 +0000

    use display implementation of enum errors in for response body. add basic implementation for poisonError.

--- src/errors.rs
+++ src/errors.rs
@@ -1,25 +1,49 @@
+use std::sync::PoisonError;
+
+use anyhow::anyhow;
+use anyhow::bail;
 use axum::{
     body::Body,
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use matchit::MatchError;
+use redis::RedisError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum LimiterError {
+    #[error("No match found")]
+    NoRouteMatch(#[from] MatchError),
+
+    /* #[error("redis error")]
+    RedisError(#[from] RedisError), */
     #[error(transparent)]
     Unknown(#[from] anyhow::Error),
 }
 
 impl IntoResponse for LimiterError {
     fn into_response(self) -> Response<Body> {
-        let body = match self {
+        /* let body = match self {
+            LimiterError::NoRouteMatch(err) => {
+                println!("No match found {:?}", err);
+                "No match found"
+            }
+
             LimiterError::Unknown(error) => {
                 println!("Error :: {:?}", error);
                 "Internal Server Error"
             }
-        };
+        }; */
 
-        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+        // There will be a better way to handle those error but for the moment a 502 for all errors is accepted.
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
+    }
+}
+
+impl<T> From<PoisonError<T>> for LimiterError {
+    fn from(err: PoisonError<T>) -> Self {
+        let error = anyhow!("Poisonned lock");
+        error.into()
     }
 }
