bbd13a1e20e8eb6cfa31edeac7ca36f87f89ca4d
g-ouattara <idrissgouattara@gmail.com>
2025-08-06 10:26:34 +0000

    prefer using a hash for redis since route can contain token that do not mesh well with redis

--- src/rules.rs
+++ src/rules.rs
@@ -1,6 +1,9 @@
+use crate::rate_limiter::RateLimiterAlgorithms;
+
 pub struct Rule {
     pub key: String,                      // The key to be rate limited
-    pub endpoint: String,                 // the endpoint : simple or regexlike
+    pub route: String,                    // the endpoint : pattern like route
+    pub hash: String,                     // The hash of the pattern, used as key in cache.
     pub algorithm: RateLimiterAlgorithms, // The algorithm to use
     pub limit: u64,                       // The maximum number of requests
     pub expiration: u64,                  // The time window for the rate limit
@@ -10,22 +13,24 @@ pub fn generate_dummy_rules() -> Vec<Rule> {
     vec![
         Rule {
             key: "user1".to_string(),
-            endpoint: "/products".to_string(),
+            route: "/products".to_string(),
+            hash: "product_key".to_string(),
             algorithm: RateLimiterAlgorithms::FixedWindow,
             limit: 100,
             expiration: 60,
         },
         Rule {
             key: "user2".to_string(),
-            endpoint: "/api/v1/orders".to_string(),
+            route: "/api/v1/orders".to_string(),
+            hash: "api_key".to_string(),
             algorithm: RateLimiterAlgorithms::FixedWindow,
             limit: 50,
             expiration: 120,
         },
-        // rule wiht regex
         Rule {
             key: "user3".to_string(),
-            endpoint: r"/api/v1/users/\d+".to_string(), // regex-like endpoint
+            route: "/api/v1/users/{id}".to_string(),
+            hash: "user_key".to_string(),
             algorithm: RateLimiterAlgorithms::FixedWindow,
             limit: 200,
             expiration: 300,
