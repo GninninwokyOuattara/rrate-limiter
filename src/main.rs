f2c6158058529541c7b530fdc9d960cc927e39ca
g-ouattara <idrissgouattara@gmail.com>
2025-08-06 10:30:35 +0000

    - Changed the limiter endpoint to be catchall - initial test : from endpoint find match route then algorithm from redis.
    note : having a lock on the app state is not great.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
 
 use axum::{
     Router,
-    extract::{Query, State},
+    extract::{Query, Request, State},
     response::IntoResponse,
     routing::get,
 };
@@ -14,30 +14,42 @@ mod rate_limiter;
 mod rules;
 mod utils;
 
+use axum_macros::debug_handler;
 use matchit::Router as MatchitRouter;
+use redis::Commands;
+
+use crate::{rules::generate_dummy_rules, utils::populate_redis_kv_rule_algorithm};
 
 struct States {
     redis_connection: redis::Connection,
-    router: matchit::Router<&'static str>,
+    route_matcher: matchit::Router<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut router = MatchitRouter::new();
-    let st = String::from("/home");
-    router.insert(st, "Welcome!")?;
-    router.insert("/users/{id}".to_string(), "A User")?;
+    let mut redis_connection = connect_to_redis()?;
+    let mut route_matcher = MatchitRouter::new();
+
+    let dummy_rules = generate_dummy_rules();
+
+    populate_redis_kv_rule_algorithm(&mut redis_connection, &dummy_rules)?;
+
+    dummy_rules.into_iter().for_each(|rule| {
+        route_matcher
+            .insert(rule.route, rule.hash)
+            .expect("Failed to insert route");
+    });
 
     tracing_subscriber::fmt::init();
 
     let states = Arc::new(Mutex::new(States {
-        redis_connection: connect_to_redis()?,
-        router,
+        redis_connection: redis_connection,
+        route_matcher,
     }));
 
     let app = Router::new()
-        .route("/", get(|| async { "hello, world!" }))
-        .route("/check", get(limiter_handler))
+        .route("/", get(limiter_handler))
+        .fallback(get(limiter_handler))
         .with_state(states.clone());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
@@ -51,12 +63,34 @@ fn connect_to_redis() -> Result<redis::Connection, Box<dyn std::error::Error>> {
     Ok(redis::Client::open("redis://127.0.0.1/")?.get_connection()?)
 }
 
+#[debug_handler]
 async fn limiter_handler(
     Query(params): Query<HashMap<String, String>>,
     State(states): State<Arc<Mutex<States>>>,
+    request: Request,
 ) -> impl IntoResponse {
     println!("Received params: {:?}", params);
-    let (message, headers) = rate_limiter::RateLimiter::check(
+    let uri = request.uri();
+    let headers = request.headers();
+
+    // Finding which pattern match the uri using the matcher
+
+    let res = {
+        let lock = states.lock().unwrap();
+        lock.route_matcher.at(uri.path()).unwrap().value.clone()
+    };
+
+    let algorithm = {
+        let mut lock = states.lock();
+        /* println!("lock 2 found: {:#?}", &lock.err()); */
+        let algorithm = lock
+            .unwrap()
+            .redis_connection
+            .get::<&String, String>(&format!("rules_to_algorithms:{}", &res));
+        println!("Algorithm found: {:#?}", algorithm);
+    };
+
+    /* let (message, headers) = rate_limiter::RateLimiter::check(
         &mut states.lock().unwrap().redis_connection,
         params.get("key").unwrap_or(&"default_key".to_string()),
         params.get("endpoint").unwrap_or(&"products".to_string()),
@@ -64,5 +98,9 @@ async fn limiter_handler(
     )
     .unwrap();
 
-    (axum::http::StatusCode::OK, headers.to_headers(), message)
+    (axum::http::StatusCode::OK, headers.to_headers(), message) */
+
+    // println!("Request: {:#?}", request);
+
+    "Hello World!"
 }
