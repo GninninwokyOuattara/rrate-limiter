f230289a72b5d03e14b7ba52ebd4d069e531d0ca
g-ouattara <idrissgouattara@gmail.com>
2025-08-09 23:11:29 +0000

    parking_lot coming in clutch. but performance still not it.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
-
 use anyhow::anyhow;
 use axum::{
     Router,
-    extract::{Query, Request, State},
+    extract::{Request, State},
     response::IntoResponse,
     routing::get,
 };
+use axum_macros::debug_handler;
+use matchit::Router as MatchitRouter;
+use parking_lot::Mutex;
+use redis::Commands;
+use std::sync::Arc;
 
 mod errors;
 mod rate_limiter;
 mod rules;
 mod utils;
-
-use axum_macros::debug_handler;
-use matchit::Router as MatchitRouter;
-use redis::Commands;
 
 use crate::{
     rate_limiter::RateLimiterAlgorithms,
@@ -28,7 +24,7 @@ use crate::{
 
 struct States {
     redis_connection: Arc<Mutex<redis::Connection>>,
-    route_matcher: Arc<Mutex<matchit::Router<String>>>,
+    route_matcher: Arc<matchit::Router<String>>,
 }
 
 #[tokio::main]
@@ -52,7 +48,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let states = Arc::new(States {
         redis_connection: Arc::new(Mutex::new(redis_connection)),
-        route_matcher: Arc::new(Mutex::new(route_matcher)),
+        route_matcher: Arc::new(route_matcher),
     });
 
     let app = Router::new()
@@ -73,29 +69,22 @@ fn connect_to_redis() -> Result<redis::Connection, Box<dyn std::error::Error>> {
 
 #[debug_handler]
 async fn limiter_handler(
-    Query(params): Query<HashMap<String, String>>,
     State(states): State<Arc<States>>,
     request: Request,
 ) -> anyhow::Result<impl IntoResponse, errors::LimiterError> {
-    println!("Received params: {:#?}", params);
     let uri = request.uri();
     let _headers = request.headers();
-    println!("Headers :: {:?}", _headers);
-    // let tracking_key = get_tracked_key_from_header(headers, tracking_type, custom_header_key)
-
-    // TODO: properly handle the key that should be rate limited.
 
     // Finding which pattern match the uri using the matcher
 
     // We use the router matcher for that
     let matched_route = states
         .route_matcher
-        .lock()?
+        .clone()
         .at(uri.path())
         .map_err(|_err| errors::LimiterError::NoRouteMatch(uri.path().to_string()))?
         .value
         .clone();
-    println!("The matching route : {:#?}", &matched_route);
 
     // We retrieve the algorithm, expiration and limit from redis
     let (rl_algo, expiration, limit, custom_tracking_key, tracking_type): (
@@ -104,7 +93,7 @@ async fn limiter_handler(
         u64,
         String,
         String,
-    ) = states.redis_connection.lock()?.hmget(
+    ) = states.redis_connection.lock().hmget(
         format!("rules:{}", matched_route),
         &[
             "algorithm",
@@ -115,41 +104,25 @@ async fn limiter_handler(
         ],
     )?;
 
-    println!(
-        "All values : {:?}",
-        (
-            &rl_algo,
-            &expiration,
-            &limit,
-            &custom_tracking_key,
-            &tracking_type
-        )
-    );
-
     let tracking_key = get_tracked_key_from_header(
         &request.headers(),
         &tracking_type.into(),
         custom_tracking_key.into(),
     )?;
 
-    println!("The tracking key extracted is :: {}", &tracking_key);
-
     // Where the rate limiting happens.
     let Ok(rate_limiting_algorithm) = RateLimiterAlgorithms::from_string(&rl_algo) else {
         return Err(anyhow!("Could not convert cache key to local algorithm").into());
     };
 
-    let (message, headers) = {
-        let mut connection = states.redis_connection.lock()?;
-        rate_limiter::RateLimiter::check(
-            &mut connection,
-            &tracking_key,
-            &matched_route,
-            rate_limiting_algorithm,
-            limit,
-            expiration,
-        )?
-    };
+    let (message, headers) = rate_limiter::RateLimiter::check(
+        &mut states.redis_connection.lock(),
+        &tracking_key,
+        &matched_route,
+        rate_limiting_algorithm,
+        limit,
+        expiration,
+    )?;
 
     Ok((axum::http::StatusCode::OK, headers.to_headers(), message))
 }
