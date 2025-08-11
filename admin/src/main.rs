7096c2831a1b00568e43ad0fbd607fd1238042cd
g-ouattara <idrissgouattara@gmail.com>
2025-08-11 16:48:21 +0000

    basic server

--- admin/src/main.rs
+++ admin/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    
+
+    let app = Router::new()
+        .route("/", get(async move || "Hello, World!"));
+        
+        
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
