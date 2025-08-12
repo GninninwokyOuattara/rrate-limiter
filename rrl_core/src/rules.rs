f327d51477b723b88a3d9f14cf254e1dbe01844f
g-ouattara <idrissgouattara@gmail.com>
2025-08-12 23:49:42 +0000

    updated the rule types to work nice with postgres

--- rrl_core/src/rules.rs
+++ rrl_core/src/rules.rs
@@ -1,4 +1,10 @@
-use tokio_postgres::Row;
+use std::time::SystemTime;
+
+use chrono::Utc;
+use tokio_postgres::{
+    Row,
+    types::{Timestamp, Type},
+};
 use uuid::Uuid;
 
 use crate::{LimiterTrackingType, RateLimiterAlgorithms};
@@ -8,28 +14,27 @@ pub struct Rule {
     pub id: String,                       // The key to be rate limited
     pub route: String,                    // the endpoint : pattern like route
     pub algorithm: RateLimiterAlgorithms, // The algorithm to use
-    pub limit: u32,                       // The maximum number of requests
-    pub expiration: u32,                  // The time window for the rate limit
+    pub limit: i32,                       // The maximum number of requests
+    pub expiration: i32,                  // The time window for the rate limit
     pub tracking_type: LimiterTrackingType,
     pub custom_tracking_key: Option<String>,
     pub status: bool,
-    pub ttl: u32,
-    pub date_creation: String,
-    pub date_modification: String,
+    pub ttl: i32,
+    pub date_creation: chrono::DateTime<Utc>,
+    pub date_modification: chrono::DateTime<Utc>,
 }
 
 impl TryFrom<Row> for Rule {
     type Error = Box<dyn std::error::Error>;
 
     fn try_from(value: Row) -> Result<Self, Self::Error> {
-        let algorithm: RateLimiterAlgorithms = value.get::<_, String>("algorithm").try_into()?;
-        let tracking_type: LimiterTrackingType =
-            value.get::<_, String>("tracking_type").try_into()?;
+        // let algorithm: RateLimiterAlgorithms = value.get::<_, String>("algorithm").try_into()?;
+
         Ok(Rule {
             id: value.get::<_, Uuid>("id").into(),
             route: value.get("route"),
-            algorithm,
-            tracking_type,
+            algorithm: value.get("algorithm"),
+            tracking_type: value.get("tracking_type"),
             limit: value.get("limit"),
             expiration: value.get("expiration"),
             custom_tracking_key: value.get("custom_tracking_key"),
