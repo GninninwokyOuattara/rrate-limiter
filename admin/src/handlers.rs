35d6194164b4d3914ae318a1b38d39131e6311f4
g-ouattara <idrissgouattara@gmail.com>
2025-08-14 12:30:13 +0000

    feature: the patch endpoint

--- admin/src/handlers.rs
+++ admin/src/handlers.rs
@@ -6,11 +6,11 @@ use axum::{
     response::IntoResponse,
 };
 use axum_macros::debug_handler;
-use rrl_core::{Rule, tokio_postgres::Client, uuid::Uuid};
+use rrl_core::{Rule, postgres_types::ToSql, tokio_postgres::Client, uuid::Uuid};
 
 use crate::{
     errors::ServiceError,
-    models::{Pagination, PostedRule},
+    models::{Pagination, PatchedRule, PostedRule},
 };
 
 #[debug_handler]
@@ -98,11 +98,115 @@ pub async fn post_rule(
 }
 
 #[debug_handler]
-pub async fn delete_rule() -> Result<impl IntoResponse, ()> {
+pub async fn delete_rule(
+    Path(rule_id): Path<Uuid>,
+    State(client): State<Arc<Client>>,
+) -> Result<impl IntoResponse, ServiceError> {
+    let _result = client
+        .query(
+            r#"
+            delete from rules where id = $1;
+            "#,
+            &[&rule_id],
+        )
+        .await?;
     Ok(())
 }
 
 #[debug_handler]
-pub async fn patch_rule() -> Result<impl IntoResponse, ()> {
-    Ok(())
+pub async fn patch_rule(
+    Path(rule_id): Path<Uuid>,
+    State(client): State<Arc<Client>>,
+    Json(rule): Json<PatchedRule>,
+) -> Result<impl IntoResponse, ServiceError> {
+    let route_field;
+    let limit_field;
+    let expiration_field;
+    let algorithm_field;
+    let tracking_type_field;
+    let custom_tracking_key_field;
+    let status_field;
+    let ttl_field;
+
+    let mut fields: Vec<String> = Vec::new();
+    let mut params: Vec<&(dyn ToSql + Sync)> = vec![];
+
+    params.push(&rule_id);
+    let mut i = 2;
+
+    if let Some(route) = rule.route {
+        route_field = route;
+        fields.push(format!("route = ${}", i));
+        params.push(&route_field);
+        i += 1;
+    }
+
+    if let Some(limit) = rule.limit {
+        fields.push(format!("limit = ${}", i));
+        limit_field = limit as i32;
+        params.push(&limit_field);
+        i += 1;
+    }
+
+    if let Some(expiration) = rule.expiration {
+        fields.push(format!("expiration = ${}", i));
+        expiration_field = expiration as i32;
+        params.push(&expiration_field);
+        i += 1;
+    }
+
+    if let Some(algorithm) = rule.algorithm {
+        fields.push(format!("algorithm = ${}", i));
+        algorithm_field = algorithm;
+        params.push(&algorithm_field);
+        i += 1;
+    }
+
+    if let Some(tracking_type) = rule.tracking_type {
+        fields.push(format!("tracking_type = ${}", i));
+        tracking_type_field = tracking_type;
+        params.push(&tracking_type_field);
+        i += 1;
+    }
+
+    if let Some(custom_tracking_key) = rule.custom_tracking_key {
+        fields.push(format!("custom_tracking_key = ${}", i));
+        custom_tracking_key_field = custom_tracking_key;
+        params.push(&custom_tracking_key_field);
+        i += 1;
+    }
+
+    if let Some(status) = rule.status {
+        fields.push(format!("status = ${}", i));
+        status_field = status;
+        params.push(&status_field);
+        i += 1;
+    }
+
+    if let Some(ttl) = rule.ttl {
+        fields.push(format!("ttl = ${}", i));
+        ttl_field = ttl as i32;
+        params.push(&ttl_field);
+        // i += 1;
+    }
+    fields.push(format!("date_modification = now()"));
+
+    let query_string = format!(
+        "update rules set {} where id = $1 returning *;",
+        fields.join(", ")
+    );
+
+    let result = client.query(&query_string, &params).await?;
+
+    if result.len() == 0 {
+        return Err(ServiceError::RuleNotFound(rule_id));
+    }
+
+    let rule: Rule = result
+        .get(0)
+        .ok_or_else(|| ServiceError::RuleNotFound(rule_id))?
+        .to_owned()
+        .try_into()?;
+
+    Ok(Json(rule))
 }
--- admin/src/models.rs
+++ admin/src/models.rs
@@ -19,3 +19,15 @@ pub struct PostedRule {
     pub status: bool,
     pub ttl: u32,
 }
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PatchedRule {
+    pub route: Option<String>,
+    pub algorithm: Option<RateLimiterAlgorithms>,
+    pub tracking_type: Option<LimiterTrackingType>,
+    pub limit: Option<u32>,
+    pub expiration: Option<u32>,
+    pub custom_tracking_key: Option<String>,
+    pub status: Option<bool>,
+    pub ttl: Option<u32>,
+}
