5a070aeccd6948a8b269cced328b7bf50344e974
g-ouattara <idrissgouattara@gmail.com>
2025-08-18 22:24:09 +0000

    watcher: include the id too

--- watcher/src/main.rs
+++ watcher/src/main.rs
@@ -106,6 +106,7 @@ fn make_redis_script(rules: Vec<Rule>) -> Script {
 
     rules.into_iter().for_each(|rule| {
         let id = rule.id;
+        let id_field = format!("{}{}{}", r#""id":""#, id.clone(), r#"""#);
         let route = format!("{}{}{}", r#""route":""#, rule.route, r#"""#);
         let algorithm = format!(
             "{}{}{}",
@@ -133,7 +134,7 @@ fn make_redis_script(rules: Vec<Rule>) -> Script {
 
         let script = format!(
             r"
-            redis.call('JSON.SET', 'rules', {key} , {} {route}, {algorithm}, {limit}, {expiration}, {tracking_type}, {custom_tracking_key}, {status} {})
+            redis.call('JSON.SET', 'rules', {key} , {} {id_field}, {route}, {algorithm}, {limit}, {expiration}, {tracking_type}, {custom_tracking_key}, {status} {})
             ", r#"'{"#, r#"}'"#
         );
 
